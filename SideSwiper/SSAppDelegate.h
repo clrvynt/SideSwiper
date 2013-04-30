6d6a5a8287f4d1b1505973204ab8ddca82e343b6
kal <kalyan.krishnamurthi@biascorp.com>
2013-04-30 10:57:55 -0500

    Initial Commit

---
+++ SideSwiper/SSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SSAppDelegate.h
+//  SideSwiper
+//
+//  Created by kalyankrishnamurthi on 4/30/13.
+//  Copyright (c) 2013 kalyankrishnamurthi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SideSwiper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SideSwiper
+//
+//  Created by kalyankrishnamurthi on 4/30/13.
+//  Copyright (c) 2013 kalyankrishnamurthi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SSAppDelegate class]));
+    }
+}
